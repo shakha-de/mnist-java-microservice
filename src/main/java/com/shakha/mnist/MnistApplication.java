717c2f9f043b59e5b3a477fad9f49aa7fbfb4275
Shakhriyor Kadamboev <shakhriyor.kadamboev@student.uni-halle.de>
2025-09-20 18:29:12 +0200

    initial package

---
+++ src/main/java/com/shakha/mnist/MnistApplication.java
@@ -0,0 +1,17 @@
+package com.shakha.mnist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class MnistApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MnistApplication.class, args);
+    }
+
+
+
+
+}
---
+++ src/test/java/com/shakha/mnist/MnistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shakha.mnist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MnistApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
